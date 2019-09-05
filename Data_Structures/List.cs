5aee62c8d5326b9cdc8fb454abbef70e8ad1cab0
jjackson20 <jamesjackson@quickenloans.com>
2019-09-04 21:34:56 -0400

    data structures

---
+++ Data_Structures/List.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures
+{
+    public class List<T> where T : IComparable, IEquatable<T>
+    {
+        const int DEFAULT_ARRAY_LENGTH = 4;
+
+
+
+        private T[] _items;
+
+        private int _count;
+        private int _currentLength = DEFAULT_ARRAY_LENGTH;
+
+        public List()
+        {
+            _items = new T[DEFAULT_ARRAY_LENGTH];
+        }
+
+        public List(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("Your capicity cannot be lower than 0");
+            }
+
+            _items = new T[capacity];
+        }
+
+        public int Count => _count;
+
+        public void Add(T item)
+        {
+            var position = _count;
+
+            if (position >= _items.Length)
+            {
+                HandleArrayResize();
+            }
+
+            _items[position] = item;
+            _count = position + 1;
+        }
+
+        private void HandleArrayResize()
+        {
+
+            var currentArray = _items;
+            _items = new T[_currentLength * 2];
+            currentArray.CopyTo(_items, 0);
+
+            _currentLength *= 2;
+        }
+    }
+}
---
+++ Data_Structures/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // STack 
+            var nameStack = new Stack<string>();
+
+            nameStack.Push("James");
+            nameStack.Push("Rob");
+            var name = nameStack.Pop();
+
+            Console.WriteLine(nameStack.Peek());
+
+
+            var sOS = new Queue<string>();
+
+            sOS.Enqueue("James");
+            sOS.Enqueue("Rob");
+            var currentCustomer = sOS.Dequeue();
+
+            Console.WriteLine(sOS.Peek());
+        }
+    }
+}
