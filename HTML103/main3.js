5ff992f8a257c90642b708672df997b0b2091e61
Jackson <jamesjackson@quickenloans.com>
2019-09-18 19:22:14 -0400

    jerver script

--- HTML103/main3.js
+++ HTML103/main3.js
@@ -1,8 +1,43 @@
+var form = document.getElementById('list-form');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const listElement = document.getElementById("form-list");
+    const inputElement = document.getElementById("user-input");
+
+    const value = inputElement.value;
+
+    const listItem = document.createElement("li");
+    listItem.innerText = value;
+
+    listElement.appendChild(listItem);
+
+});
+
+
 function clickHandler() {
+
+    console.log(document);
+
     const listElement = document.getElementById("list");
+
+    const children = document.children;
+
+    console.log(JSON.stringify(listElement));
+
+    console.log(children);
 
     const listItem = document.createElement('li');
     listItem.innerText = "sup";
 
     listElement.appendChild(listItem);
+}
+
+
+
+
+function formHandler(e) {
+    e.preventDefault();
 }
