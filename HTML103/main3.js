87e2d3c9405ee54edd05c7b49baa1b21b9de9fec
Jackson <jamesjackson@quickenloans.com>
2019-09-19 09:38:13 -0400

    console logs for notes

--- HTML103/main3.js
+++ HTML103/main3.js
@@ -5,14 +5,30 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     
     const listElement = document.getElementById("form-list");
+
+    
+
+    console.log("OUR CAPTURED EXISTING DOM ELEMENT")
+    console.log(listElement)
+
     const inputElement = document.getElementById("user-input");
 
     const value = inputElement.value;
 
     const listItem = document.createElement("li");
+
+    console.log("vvv THIS GUY DOES NOT EXIST IN THE DOM YET")
+    console.log(listItem);
+
     listItem.innerText = value;
 
+    console.log("vvv STILL NOT IN THE DOM, BUT HAS INNER TEXT")
+    console.log(listItem);
+
     listElement.appendChild(listItem);
+
+    console.log("vvv OUR LIST ELEMENT THAT EXIST IN THE DOM, NOW HAS AN ADDITIONAL CHILD ELEMENT")
+    console.log(listElement);
 
 });
 
@@ -41,3 +57,22 @@ function clickHandler() {
 function formHandler(e) {
     e.preventDefault();
 }
+
+
+// jQuery way
+const jqueryElement = $("#id");
+
+// javascript way
+const element = document.querySelector("#id");
+
+
+// jQuery event binding
+jqueryElement.on("click", (e) => {
+    // code here
+})
+
+
+// javascript way
+element.addEventListener("click", (e) => {
+    // code here
+})
